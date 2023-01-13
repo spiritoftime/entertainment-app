bff4938064ac09f622a22ba8695e7c6f36891246
spiritoftime <chooliangyi6@gmail.com>
2023-01-13 11:43:27 +0800

    add react routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,13 @@ import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage/HomePage";
+import Layout from "./shared components/Layout";
 let allMovies = [];
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<Layout />} />
+      <Route index element={<HomePage />} />
     </Routes>
   );
 }
